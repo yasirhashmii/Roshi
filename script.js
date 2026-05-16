d57c9ef5a88b81c0da29bce5aea57bb1abea2c48
Mohd Yasir Hashmi <yasirhashmi324@gmail.com>
2026-05-16 14:37:52 +0530

    Made multiple changes to the site. Added pictures, made the site darker.

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ function createHeart() {
     document.getElementById('heart-container').appendChild(heart);
     setTimeout(() => { heart.remove(); }, 6000);
 }
-setInterval(createHeart, 1000);
+setInterval(createHeart, 3000);
+
+// function createCutie() {
+//     const heart = document.createElement('div');
+//     heart.classList.add('heart');
+//     heart.innerHTML = 'Cutie';
+//     heart.style.left = Math.random() * 100 + 'vw';
+//     heart.style.animationDuration = Math.random() * 3 + 4 + 's'; 
+//     heart.style.fontSize = Math.random() * 1 + 1 + 'rem'; 
+//     document.getElementById('heart-container').appendChild(heart);
+//     setTimeout(() => { heart.remove(); }, 6000);
+// }
+// setInterval(createCutie, 3000);
 
 
 // 1. Generate or retrieve a unique ID for this device
